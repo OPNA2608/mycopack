4426d1b3b66a44d98b4eb6039ac4b5213c0a25f2
OPNA2608 <opna2608@protonmail.com>
2025-07-14 23:14:33 +0200

    Animate roachard, add element callbacks, fix tab order

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,15 +1,70 @@
 #include "mainwindow.h"
 #include "../ui/ui_mainwindow.h"
 
+#include <QMovie>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // roachard dancing gif
+    QMovie* roachDance = new QMovie (":/images/roachard-dance.gif");
+    if (!roachDance->isValid()) {
+        qDebug() << "Failed to load funny roach dance GIF :(";
+        return;
+    }
+
+    auto roachardLabel = findChild<QLabel*> ("roachardLabel");
+    if (!roachardLabel) {
+        qDebug() << "Failed to find funny roach dance label :(";
+        return;
+    }
+
+    roachardLabel->setMovie (roachDance);
+    roachDance->start();
+    qDebug() << "Roach has been deployed!";
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_listAddButton_clicked()
+{
+    qDebug() << "Pressed Add button.";
+}
+
+
+void MainWindow::on_listChangeButton_clicked()
+{
+    qDebug() << "Pressed Change button.";
+}
+
+
+void MainWindow::on_listRemoveButton_clicked()
+{
+    qDebug() << "Pressed Remove button.";
+}
+
+
+void MainWindow::on_detailsColourButton_clicked()
+{
+    qDebug() << "Pressed Colour button.";
+}
+
+
+void MainWindow::on_packButton_clicked()
+{
+    qDebug() << "Pressed Pack button.";
+}
+
+
+void MainWindow::on_listWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous)
+{
+    qDebug() << "Focus of list widget changed to element " << current;
 }
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidgetItem>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,19 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_listAddButton_clicked();
+
+    void on_listChangeButton_clicked();
+
+    void on_listRemoveButton_clicked();
+
+    void on_detailsColourButton_clicked();
+
+    void on_packButton_clicked();
+
+    void on_listWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
 
 private:
     Ui::MainWindow *ui;
